669c4b7ae71eb2975e1dee91c01b628061b83f78
Andrei Todiras <st188776@stud.uni-stuttgart.de>
2025-03-25 11:28:22 +0100

    1.7-1.9 done

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,64 +1,39 @@
+import { useState } from 'react'
+
+const Statistics = ({good, neutral, bad}) => {
+  if (good + neutral + bad === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  } else{
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {good + neutral + bad}</p>
+      <p>average: {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive: {good / (good + neutral + bad) * 100} %</p>
+    </div>)
+    }
+}
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    } 
-  ] 
-  }
-  return (
-    <div>
-      <Head name={course.name} />
-      <Content parts={course.parts} />
-      <Total exercises={course.parts} />
-    </div>
-  )
-}
-
-const Head = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.name} {props.exercises}</p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.parts.map(part => <Part key={part.name} name={part.name} exercises={part.exercises} />)}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const exercises1 = props.exercises[0].exercises;
-  const exercises2 = props.exercises[1].exercises;
-  const exercises3 = props.exercises[2].exercises;
-
-  const totalExercises = exercises1 + exercises2 + exercises3;
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <p>Number of exercises {totalExercises}</p>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
