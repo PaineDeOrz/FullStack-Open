9f0d069d2f0aab2f3c2410b9f6f203c7a6130e15
Andrei Todiras <st188776@stud.uni-stuttgart.de>
2025-03-25 11:42:27 +0100

    1.10 done

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -12,14 +12,38 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <div>
       <h1>Statistics</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {good + neutral + bad}</p>
-      <p>average: {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive: {good / (good + neutral + bad) * 100} %</p>
+      <StatisticsLine text='good' value={good}/>
+      <StatisticsLine text='neutral' value={neutral}/>
+      <StatisticsLine text='bad' value={bad}/>
+      <StatisticsLine text='all' value={good + neutral + bad}/>
+      <StatisticsLine text='average' value={(good - bad) / (good + neutral + bad)}/>
+      <StatisticsLine text='positive' value={(good / (good + neutral + bad)) * 100 + ' %'}/>
     </div>)
     }
+}
+
+const StatisticsLine = (props) => {
+  return(
+    <p>{props.text}: {props.value}</p>
+  )
+}
+
+const Button = (props) =>{
+  if(props.text === 'good'){
+    return(
+      <button onClick={props.onClick}>good</button>
+    )
+  }
+  if(props.text === 'neutral'){
+    return(
+      <button onClick={props.onClick}>neutral</button>
+    )
+  }
+  if(props.text === 'bad'){
+    return(
+      <button onClick={props.onClick}>bad</button>
+    )
+  }
 }
 
 const App = () => {
@@ -30,9 +54,9 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} text='good'/>
+      <Button onClick={() => setNeutral(neutral + 1)} text='neutral'/>
+      <Button onClick={() => setBad(bad + 1)} text='bad'/>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
