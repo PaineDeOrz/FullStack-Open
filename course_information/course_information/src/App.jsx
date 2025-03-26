acb27420fba7286fa2c21da3d789cd55d1f9a199
Andrei Todiras <st188776@stud.uni-stuttgart.de>
2025-03-26 12:14:20 +0100

    2.5 done

--- course_information/course_information/src/App.jsx
+++ course_information/course_information/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from './components/Course'
+
 const App = () => {
   const courses = [
     {
@@ -49,37 +51,6 @@ const App = () => {
       {courses.map(course => <Course key={course.id} course={course} />)}
     </div>
   )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
-
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-
-const Part = ({ part }) => {
-  return <p>{part.name} {part.exercises}</p>
-}
-
-const Total = ({ course }) => {
-  const total = course.parts.reduce((acc, part) => acc + part.exercises, 0)
-  return <p><strong>total of {total} exercises</strong></p>
 }
 
 export default App
