d5ad195312559e45775ab50fe83736e91f738dcf
Andrei Todiras <st188776@stud.uni-stuttgart.de>
2025-03-26 13:47:41 +0100

    2.9 done

--- phone_book/phone_book/src/App.jsx
+++ phone_book/phone_book/src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -35,9 +37,22 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setShowAll(false)
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = showAll
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
@@ -46,7 +61,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {personsToShow.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+      </ul>
     </div>
   )
 }
