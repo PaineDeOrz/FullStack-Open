ab89de7c133080f37e07dfc6702b8eefead16f2f
Andrei Todiras <st188776@stud.uni-stuttgart.de>
2025-03-26 13:33:11 +0100

    2.7, 2.8 done

--- phone_book/phone_book/src/App.jsx
+++ phone_book/phone_book/src/App.jsx
@@ -2,33 +2,45 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
-    const personObject = {name: newName}
+    const personObject = {name: newName, number: newNumber}
+    if (newName === '' || newNumber === '') {
+      alert('Name or number cannot be empty')
+      return
+    }
     if (persons.find(person => person.name === newName)) {
       alert(`${newName} is already added to the phonebook`)
+      return
+    }
+    if (persons.find(person => person.number === newNumber)) {
+      alert(`${newNumber} is already added to the phonebook`)
       return
     }
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input onChange={handleNameChange}/>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
