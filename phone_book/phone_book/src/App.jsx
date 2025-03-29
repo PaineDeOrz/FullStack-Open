8edef50a9166d130181eafd3054d63f574d44794
Andrei Todiras <st188776@stud.uni-stuttgart.de>
2025-03-29 17:38:27 +0100

    2.16 done

--- phone_book/phone_book/src/App.jsx
+++ phone_book/phone_book/src/App.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import {useState, useEffect} from 'react'
 import personService from './services/persons'
+import './index.css'
 
 const Filter = ({filter, handleFilterChange}) => {
   return (
@@ -34,12 +35,24 @@ const Persons = ({personsToShow, deletePerson}) => {
   )
 }
 
+const Notification = ({message}) => {
+  if (message === null) {
+    return null
+  }
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [filter, setFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect( () => {
     personService
@@ -71,6 +84,10 @@ const App = () => {
               setNewName('')
               setNewNumber('')
             })
+            setErrorMessage(`Updated ${newName}`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
         }
       }
       else {alert(`${newName} is already added to the phonebook`)}
@@ -85,6 +102,10 @@ const App = () => {
         setNewName('')
         setNewNumber('')
       })
+      setErrorMessage(`Added ${newName}`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
   } 
 
   const deletePerson = (id) => {
@@ -116,6 +137,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage}/>
       <Filter filter={filter} handleFilterChange={handleFilterChange}/>
       <h2>Add a new</h2>
       <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
