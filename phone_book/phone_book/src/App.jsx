2d70a410add146dde6df3b4939c7b2dff96310f5
Andrei Todiras <st188776@stud.uni-stuttgart.de>
2025-03-29 11:11:01 +0100

    2.11 done

--- phone_book/phone_book/src/App.jsx
+++ phone_book/phone_book/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
 const Filter = ({filter, handleFilterChange}) => {
   return (
@@ -29,9 +30,7 @@ const Persons = ({personsToShow}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showAll, setShowAll] = useState(true)
@@ -74,6 +73,13 @@ const App = () => {
     ? persons
     : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
+  useEffect( () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  },[])
   return (
     <div>
       <h2>Phonebook</h2>
